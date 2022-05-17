48430024967a98fb40cd476f149b571216f42b6a
JongMin22 <bbgs0605@naver.com>
2022-05-17 16:10:12 +0900

    2022-05-17

--- security_prj/src/main/java/com/ict/domain/AuthVO.java
+++
@@ -1,9 +0,0 @@
-package com.ict.domain;
-
-import lombok.Data;
-
-@Data
-public class AuthVO {
-	private String userId;
-	private String auth;
-}
--- security_prj/src/main/java/com/ict/service/SecurityService.java
+++
@@ -1,7 +0,0 @@
-package com.ict.service;
-
-import com.ict.domain.MemberVO;
-
-public interface SecurityService {
-	public void insertMember(MemberVO vo);
-}
